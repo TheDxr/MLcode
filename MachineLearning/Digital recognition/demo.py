e9659d8858ac51d25a638d75bdbfc5353ca35f01
TheDxr <872151901@qq.com>
2020-03-30 20:56:43 +0800

    MNIST数字识别

--- MachineLearning/Digital recognition/demo.py
+++ MachineLearning/Digital recognition/demo.py
@@ -1,37 +1,39 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-import gzip
+import network
 import struct
 import matplotlib.pyplot as plt
-import network
 
-def read_data(train_path,test_path):
+
+def read_data(train_path, test_path):
     """
     读取MNIST数据集
     `return` tuple(数据集,标签)
     """
-    data = open(train_path,'rb').read()
-    data2 = open(test_path,'rb').read()
+    data = open(train_path, 'rb').read()
+    data2 = open(test_path, 'rb').read()
     index = 0
     index2 = 8
     ret_data = []
-    magic, num, width, height = struct.unpack_from('>IIII',data,index)
+    magic, num, width, height = struct.unpack_from('>IIII', data, index)
     index += struct.calcsize('>IIII')
     for i in range(num):
-        img = struct.unpack_from('%dB'%(height*width),data,index)
-        lable = struct.unpack_from('B',data2,index2)
-        index += struct.calcsize('%dB'%(height*width))
+        img = struct.unpack_from('%dB' % (height * width), data, index)
+        lable = struct.unpack_from('B', data2, index2)
+        index += struct.calcsize('%dB' % (height * width))
         index2 += 1
-        ret_data.append( [np.array(img).reshape(width*height,1),int(lable[0])] )
+        ret_data.append([np.array([img]).transpose() / 255, int(lable[0])])
+        # ret_data.append( [np.array(img).reshape(width*height,1),int(lable[0])] )
     return ret_data
 
+
 if __name__ == "__main__":
-    training_data = read_data("MachineLearning/Digital recognition/DataSet/train-images.idx3-ubyte", \
-    "MachineLearning/Digital recognition/DataSet/train-labels.idx1-ubyte")
-    test_data = read_data('MachineLearning/Digital recognition/DataSet/t10k-images.idx3-ubyte',\
-        "MachineLearning/Digital recognition/DataSet/t10k-labels.idx1-ubyte")
+    training_data = read_data("D:/Code/MLcode/MachineLearning/Digital recognition/DataSet/train-images.idx3-ubyte",
+                              "D:/Code/MLcode/MachineLearning/Digital recognition/DataSet/train-labels.idx1-ubyte")
+    test_data = read_data("D:/Code/MLcode/MachineLearning/Digital recognition/DataSet/t10k-images.idx3-ubyte",
+                          "D:/Code/MLcode/MachineLearning/Digital recognition/DataSet/t10k-labels.idx1-ubyte")
     net = network.Network([784, 30, 10])
-    net.SGD(training_data, 20, 10, 3.0, test_data=test_data)
+    net.SGD(training_data, 20, 10, 3, test_data)
     # plt.figure()
     # for i in range(0,1):
     #     plt.subplot((251 + i))
--- MachineLearning/Digital recognition/network.py
+++ MachineLearning/Digital recognition/network.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-import random
 import struct
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 class Network(object):
 
@@ -23,22 +23,25 @@ class Network(object):
             a = sigmoid(np.dot(w, a)+b)
         return a
 
-    def SGD(self, training_data, epochs, mini_batch_size, eta,test_data=None):
+    def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
         """用mini-batch stochasticgradient descent训练神经网络.
         `training_data为元祖`(x, y)表示输入和输出.  
         `epoch表示迭代次数,.
         `mini_batch_size`表示取样块的大小"""
-        if test_data: n_test = len(test_data)
+        if test_data:
+            n_test = len(test_data)
         n = len(training_data)
         for j in range(epochs):
-            random.shuffle(training_data)
-            mini_batches = [training_data[k:k+mini_batch_size]for k in range(0, n, mini_batch_size)]
+            np.random.shuffle(training_data)
+            mini_batches = [training_data[k:k+mini_batch_size]
+                            for k in range(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
             if test_data:
-                print( "迭代次数 {0}: {1} / {2}".format(j, self.evaluate(test_data), n_test) )
+                print("epoch {0}: {1} / {2}".format(j + 1,
+                                                   self.evaluate(test_data), n_test))
             else:
-                print( "迭代次数 {0} complete".format(j) )
+                print("epoch {0} complete".format(j + 1))
 
     def update_mini_batch(self, mini_batch, eta):
         """应用梯度下降更新神经网络,使用反向传播对单个mini batch.
@@ -49,57 +52,76 @@ class Network(object):
             delta_nabla_b, delta_nabla_w = self.backprop(x, y)
             nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
             nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-        self.weights = [w-(eta/len(mini_batch))*nw
-                        for w, nw in zip(self.weights, nabla_w)]
-        self.biases = [b-(eta/len(mini_batch))*nb
-                       for b, nb in zip(self.biases, nabla_b)]
+
+        self.weights = [w-(eta/len(mini_batch))*nw for w,
+                        nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(eta/len(mini_batch))*nb for b,
+                       nb in zip(self.biases, nabla_b)]
 
     def backprop(self, x, y):
-        """Return:``(nabla_b, nabla_w)`` representing the
-        gradient for the cost function C_x.  ``nabla_b`` and
-        ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
-        to ``self.biases`` and ``self.weights``."""
+        """
+        反向传播计算梯度
+        :param x: 单个输入
+        :param y: 期望输出
+        :return: 梯度
+        """
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         # feedforward
         activation = x
-        activations = [x] # list to store all the activations, layer by layer
-        zs = [] # list to store all the z vectors, layer by layer
+        # list to store all the activations, layer by layer
+        activations = [np.array(x)]
+        zs = []  # list to store all the z vectors, layer by layer
         for b, w in zip(self.biases, self.weights):
             z = np.dot(w, activation)+b
             zs.append(z)
             activation = sigmoid(z)
             activations.append(activation)
         # backward pass
-        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
+        delta = self.cost_derivative(
+            activations[-1], y) * sigmoid_prime(zs[-1])
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
-        # Note that the variable l in the loop below is used a little
-        # differently to the notation in Chapter 2 of the book.  Here,
-        # l = 1 means the last layer of neurons, l = 2 is the
-        # second-last layer, and so on.  It's a renumbering of the
-        # scheme in the book, used here to take advantage of the fact
-        # that Python can use negative indices in lists.
         for l in range(2, self.num_layers):
             z = zs[-l]
-            sp = sigmoid_prime(z)
-            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+            delta = np.dot(
+                self.weights[-l+1].transpose(), delta) * sigmoid_prime(z)
             nabla_b[-l] = delta
             nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
-        return (nabla_b, nabla_w)
+        return nabla_b, nabla_w
 
     def evaluate(self, test_data):
         """Return the number of test inputs for which the neural
         network outputs the correct result. Note that the neural
         network's output is assumed to be the index of whichever
         neuron in the final layer has the highest activation."""
-        test_results = [(np.argmax(self.feedforward(x)), y) for (x, y) in test_data]
+        test_results = [(np.argmax(self.feedforward(x)), y)
+                        for x, y in test_data]
         return sum(int(x == y) for (x, y) in test_results)
 
-    def cost_derivative(self, output_activations, y):
-        """Return the vector of partial derivatives \partial C_x /
-        \partial a for the output activations."""
-        return (output_activations-y)
+    # @staticmethod
+    # def cost_derivative(output_activations, y):
+    #     """Return the vector of partial derivatives \partial C_x /
+    #     \partial a for the output activations."""
+    #     return (output_activations-y)
+    
+    def cost_derivative(self,output_activation, y):
+        """
+        :param output_activation: 网络的实际输出，是一个十维的向量，0-9 的激活值
+        :param y: 预期输出：在这里是一个整数值：0-9
+        :return: 返回一个代表差异的向量，分别表示输出层各个节点的误差，正值表示比期望大，负值表示比期望小，
+                 绝对值表示偏离期望的程度（修改的优先级）
+        此处采用二次代价：
+        对于单个样本：
+        二次代价 C_x = \frac{(y-a)^2}{2}
+                a = \sigma(z)
+                \delta^L= a - y
+        """
+        # 由于期望输出向量是[0,0,0,0,0,0,0,0,0,0][y]=1,
+        # 这里避免额外的向量计算，直接计算 output_activation - 期望输出向量
+        #output_activation = output_activation[:]
+        output_activation[y] -= 1
+        return output_activation
 
 #### Miscellaneous functions
 def sigmoid(z):
@@ -110,3 +132,11 @@ def sigmoid(z):
 def sigmoid_prime(z):
     """sigmoid函数的导数"""
     return sigmoid(z)*(1-sigmoid(z))
+
+def debug(dbg):
+    try:
+        f = open('debug.txt','a+')
+        print(dbg,file=f,end='')
+        print('\n------------------------------',file = f)
+    finally:
+        if f: f.close()
